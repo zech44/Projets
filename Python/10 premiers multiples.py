a271c03697b5f4e76887add190ca3eb2ab0d8fda
Ezéchiel N'DRI <125416353+zech44@users.noreply.github.com>
2025-05-28 11:51:13 +0000

    Delete Python directory

--- Python/10 premiers multiples.py
+++
@@ -1,7 +0,0 @@
-from math import *
-#print
-n = int(input(" saississez un nombre n = "))
-print(" les 10 premiers multiples de ",n, " sont : ")
-for i in range(0,10,1):
-   m = n * i
-   print(n," * ",i," = ",m)
